c7c7b6599b5c14559963eaa8eeda33e4ebf7394e
Jrenoth Misquith <50324377+lowkeyparanoia@users.noreply.github.com>
2023-02-06 00:45:07 +0800

    Update 3
    Data Cleaning, correcting data types and plotting out data, their categories and prices side by side.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import numpy as np
+import plotly
+plotly.offline.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import seaborn as sns
+sns.set_style("darkgrid")
+import warnings
+warnings.filterwarnings("ignore")
+
+
 apps_with_duplicates = pd.read_csv('./datasets/apps.csv')
 
 # Drop duplicates from apps_with_duplicates
@@ -9,6 +19,7 @@ print('Total number of apps in the dataset = ', apps.shape[0])
 
 # Have a look at a random sample of 5 rows
 print(apps.sample(5))
+###
 
 chars_to_remove = ['+', ',', '$']
 # List of column names to clean
@@ -23,3 +34,85 @@ for col in cols_to_clean:
         
 # Print a summary of the apps dataframe
 print(apps.info())
+#####
+
+# Convert Installs to float data type
+apps['Installs'] = apps['Installs'].astype('float')
+
+# Convert Price to float data type
+apps['Price'] = apps['Price'].astype('float')
+
+# Checking dtypes of the apps dataframe
+print(apps.info())
+
+# Print the total number of unique categories
+num_categories = len(apps['Category'].unique())
+print('Number of categories = ', num_categories)
+
+# Count the number of apps in each 'Category'. 
+num_apps_in_category = apps['Category'].value_counts()
+
+# Sort num_apps_in_category in descending order based on the count of apps in each category
+sorted_num_apps_in_category = num_apps_in_category.sort_values(ascending = False)
+
+data = [go.Bar(
+        x = num_apps_in_category.index, # index = category name
+        y = num_apps_in_category.values, # value = count
+)]
+
+plotly.offline.iplot(data)
+# Average rating of apps
+avg_app_rating = apps['Rating'].mean()
+print('Average app rating = ', avg_app_rating)
+
+# Distribution of apps according to their ratings
+data = [go.Histogram(
+        x = apps['Rating']
+)]
+
+# Vertical dashed line to indicate the average app rating
+layout = {'shapes': [{
+              'type' :'line',
+              'x0': avg_app_rating,
+              'y0': 0,
+              'x1': avg_app_rating,
+              'y1': 1000,
+              'line': { 'dash': 'dashdot'}
+          }]
+          }
+
+plotly.offline.iplot({'data': data, 'layout': layout})
+
+###
+
+# Select rows where both 'Rating' and 'Size' values are present (ie. the two values are not null)
+apps_with_size_and_rating_present = apps[apps.Rating.notnull() & apps.Size.notnull()]
+
+# Subset for categories with at least 250 apps
+large_categories = apps_with_size_and_rating_present.groupby('Category').filter(lambda x: len(x) >= 250)
+
+# Plot size vs. rating
+plt1 = sns.jointplot(x = large_categories['Size'], y = large_categories['Rating'])
+
+# Select apps whose 'Type' is 'Paid'
+paid_apps = apps_with_size_and_rating_present[apps_with_size_and_rating_present['Type'] == 'Paid']
+
+# Plot price vs. rating
+plt2 = sns.jointplot(x = paid_apps['Price'], y = paid_apps['Rating'])
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 8)
+
+# Select a few popular app categories
+popular_app_cats = apps[apps.Category.isin(['GAME', 'FAMILY', 'PHOTOGRAPHY',
+                                            'MEDICAL', 'TOOLS', 'FINANCE',
+                                            'LIFESTYLE','BUSINESS'])]
+
+# Examine the price trend by plotting Price vs Category
+ax = sns.stripplot(x = popular_app_cats['Price'], y = popular_app_cats['Category'], jitter=True, linewidth=1)
+ax.set_title('App pricing trend across categories')
+
+# Apps whose Price is greater than 200
+apps_above_200 = apps[apps['Price'] > 200]
+apps_above_200[['Category', 'App', 'Price']]
