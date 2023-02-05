805f3f8346df13e7a7e241c3ebe06d920ef55f28
Jrenoth Misquith <50324377+lowkeyparanoia@users.noreply.github.com>
2023-02-06 00:16:11 +0800

    Update 2
    Cleaning and removing unnecessary characters.

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ print('Total number of apps in the dataset = ', apps.shape[0])
 
 # Have a look at a random sample of 5 rows
 print(apps.sample(5))
+
+chars_to_remove = ['+', ',', '$']
+# List of column names to clean
+cols_to_clean = ['Installs', 'Price']
+
+# Loop for each column in cols_to_clean
+for col in cols_to_clean:
+    # Loop for each char in chars_to_remove
+    for char in chars_to_remove:
+        # Replace the character with an empty string
+        apps[col] = apps[col].apply(lambda x: x.replace(char, ''))
+        
+# Print a summary of the apps dataframe
+print(apps.info())
